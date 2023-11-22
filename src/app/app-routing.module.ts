318dcac2dbae01d28f9f33dbede06e4a7aed3b97
Paulo Solipa <psolipa@product-league.com>
2023-11-22 22:32:28 +0000

    Variables added. Web Header component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,7 +35,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, useHash: true })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules})
   ],
   exports: [RouterModule]
 })
--- src/app/components/commoncomponents.module.ts
+++ src/app/components/commoncomponents.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { BottombarComponent } from "./bottombar/bottombar.component";
 import { IonicModule } from "@ionic/angular";
+import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
-    declarations: [BottombarComponent],
-    exports: [BottombarComponent],
+    declarations: [BottombarComponent, HeaderComponent],
+    exports: [BottombarComponent, HeaderComponent],
     imports: [IonicModule]
 })
 
